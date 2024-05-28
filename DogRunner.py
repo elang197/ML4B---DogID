0b404f6308e84d1b4b7e3596b330e3ace03ed950
elang197 <168003872+elang197@users.noreply.github.com>
2024-05-28 22:39:26 +0200

    Update DogRunner.py

--- DogRunner.py
+++ DogRunner.py
@@ -5,23 +5,15 @@ from keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 import pickle
 
-# Lade das trainierte Modell und den LabelEncoder
+# das trainierte Modell und den LabelEncoder laden
 model = load_model('/Users/erwinlang/PycharmProjects/DogID/dog_breed_classifier.h5')
 with open('/Users/erwinlang/PycharmProjects/DogID/label_encoder.pkl', 'rb') as f:
     label_encoder = pickle.load(f)
 
 
 def predict_breed(image, model, label_encoder):
-    """
-    Vorhersage der Hunderasse basierend auf dem Bild.
-
-    :param image: Das Bild des Hundes als numpy Array
-    :param model: Das geladene Keras Modell
-    :param label_encoder: Der LabelEncoder für die Hunderassen
-    :return: Die vorhergesagte Hunderasse
-    """
-    image = image.resize((128, 128))  # Ändere die Bildgröße entsprechend der Modellanforderungen
-    image = np.expand_dims(np.array(image), axis=0)  # Füge Batch-Dimension hinzu
+    image = image.resize((128, 128)) 
+    image = np.expand_dims(np.array(image), axis=0) 
     predictions = model.predict(image)
     predicted_breed = label_encoder.inverse_transform([np.argmax(predictions)])
     return predicted_breed[0]
